1721d01bf55714d8fc8a2a27bc7a3b0f3a72835e
Lâm giáo sư <nghiemkhaclam185.mta@gmail.com>
2025-04-10 00:10:37 +0700

    add Lịch sử giao dịch model

---
+++ src/models/LichSuGiaoDichModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const moment = require('moment-timezone')
+
+const LichSuGiaoDich = new Schema({
+    customerId: { type: String, required: true },
+    soTienGiaoDich: { type: Number, required: true },
+    tieuDeGiaoDich: { type: String, required: true },
+    noiDungGiaoDich: { type: String, required: true },
+    type: { type: Number, required: true }, //0: Tiền vào tài khoản (+), 1: Tiền ra khỏi tài khoản (-)
+    isDelete: { type: Boolean, default: false },
+    createdAt: {
+        type: String,
+        default: () => moment.tz(Date.now(), 'Asia/Ho_Chi_Minh').format(), // Tự động lưu với múi giờ +7
+    },
+    updatedAt: {
+        type: String,
+        default: () => moment.tz(Date.now(), 'Asia/Ho_Chi_Minh').format(), // Tự động lưu với múi giờ +7
+    },
+}, {
+    timestamps: false
+})
+
+LichSuGiaoDich.pre(['updateOne', 'findOneAndUpdate'], function(next) {
+    const now = moment.tz(Date.now(), 'Asia/Ho_Chi_Minh').format();
+    this.set({ updatedAt: now }); // Cập nhật trường updatedAt với thời gian hiện tại ở múi giờ Việt Nam
+    next();
+});
+
+module.exports = mongoose.model('lichSuGiaoDich', LichSuGiaoDich)
